742b906194f7bfe03ad4ec8452048c0c162aa32f
Jiwon_83 <gkswldnj0803@gmail.com>
2022-02-11 18:11:43 +0900

    회원가입 예외처리 2022-2-11

---
+++ app/src/main/java/com/appwithme/certify/MainActivity2.java
@@ -0,0 +1,54 @@
+package com.appwithme.certify;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.appwithme.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    FragmentManager fragmentManager = getSupportFragmentManager();//1
+    LoginStartFragment loginStartFragment = new LoginStartFragment();//2
+    JoinStartFragment joinstartfragment = new JoinStartFragment(); //3 new
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();//3
+        transaction.replace(R.id.main_content2, loginStartFragment).commitAllowingStateLoss();//실제 프래그 먼트 전환하는 코드
+
+//        //만약 address값이 있다면 joinstratfragment로 전환
+//        try {
+//            Intent intent  = new Intent();
+//            if (intent.getExtras().getString("address") !=null){
+//                transaction.replace(R.id.main_content2, joinstartfragment).commitAllowingStateLoss();
+//
+//            }
+//        }catch (NullPointerException e){
+//
+//        }
+
+    }
+
+
+
+    public void replaceFragment(Fragment fragment){
+//        getSupportFragmentManager().beginTransaction().replace(R.id.main_content2, fragment).commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_content2, fragment).commit();
+    }
+    public void addFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_content2, fragment).addToBackStack(null).commit();
+    }
+} //new
+
+
--- app/src/main/java/com/withme/navigation/OnItemClick.java
+++
@@ -1,5 +0,0 @@
-package com.withme.navigation;
-
-public interface OnItemClick {
-    void onClick(String value);
-}
